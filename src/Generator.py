2f2e3b8badb9af003c720845fabad733147edca9
Adriana Fernández <adrianafdz9@gmail.com>
2022-06-06 21:58:48 -0500

    numpad mode

--- src/Generator.py
+++ src/Generator.py
@@ -9,3 +9,22 @@ class Generator:
             for i in range(num):
                 self.chosen.append(random.choice(words))
         return self.chosen
+
+    def generateOperations(self, num):
+        text = []
+        curr_num = 0
+        toggle = True
+        operadores = ["*", "/", "+", "-", "."]
+        while curr_num < num:
+            if toggle: # genera numero
+                text.append(str(random.randint(0, 9999)))
+                curr_num += 1
+            else: # genera operador
+                oper = random.choice(operadores)
+                if oper == ".":
+                    curr_num -= 1
+                text.append(oper)
+            toggle = not toggle
+
+        text = ' '.join(text).replace(" . ", ".") # para hacer numeros con decimal
+        return text
